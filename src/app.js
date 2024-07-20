f1d7f3d0a8696404736eb763ae75fb8bb8e0eab4
Arosha Ravishan <mgaravishan@gmail.com>
2024-07-20 09:42:38 +0530

    added env file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+// const trainRoutes = require('./routes/trains');
+
+// app.use(express.json());
+
+// // MongoDB connection URI
+// const dbURI = 'mongodb+srv://mgaravishan:passwordadmin@cluster0.g1wu4gq.mongodb.net/SLR_db?retryWrites=true&w=majority&appName=Cluster0';
+
+// // Connect to MongoDB
+// mongoose.connect(dbURI)
+//     .then(() => {
+//         console.log('Connected to Database');
+//         startServer();
+//     })
+//     .catch((error) => {
+//         console.error('Error connecting to MongoDB:', error.message);
+//         process.exit(1);
+//     });
+
+// // Use routes
+// app.use('/trains', trainRoutes);
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+// });
+
+// function startServer() {
+//     const port = process.env.PORT || 3000;
+//     app.listen(port, () => console.log(`Server started on port ${port}`));
+// }
+
+// process.on('unhandledRejection', (reason, promise) => {
+//     console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+// });
+
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -5,11 +44,11 @@ const trainRoutes = require('./routes/trains');
 
 app.use(express.json());
 
-// MongoDB connection URI
-const dbURI = 'mongodb+srv://mgaravishan:passwordadmin@cluster0.g1wu4gq.mongodb.net/SLR_db?retryWrites=true&w=majority&appName=Cluster0';
+// MongoDB connection URI from environment variable
+const dbURI = process.env.MONGO_URI;
 
 // Connect to MongoDB
-mongoose.connect(dbURI)
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to Database');
         startServer();
